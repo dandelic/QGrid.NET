1d2bd4fcae8cb1c7c2100582e1b4e9f1a5fc6d87
dandelic97 <dandelic97@gmail.com>
2024-12-29 13:14:07 +0100

    Revert "Delete QGrid.Tests directory"
    This reverts commit 21c346fedfa5ca1d5c3bf6c4ab758fb61bda4cb1.

---
+++ QGrid.Tests/Data/EmployeeDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace QGrid.Tests.Data
+{
+    public class EmployeeDTO
+    {
+        public string Name { get; set; } = null!;
+        public double Salary { get; set; }
+        public int Age { get; set; }
+        public string CompanyName { get; set; } = null!;
+        public string CompanyAddress { get; set; } = null!;
+        public double CompanyAverageSalary { get; set; }
+        public int CompanyEmployeesCount { get; set; }
+    }
+}
